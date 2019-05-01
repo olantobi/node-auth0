dd4697ff1e38763582b1bc872e5ed866d1de476f
Ebenezer Olanrewaju <ebenezer.olanrewaju@venturegardengroup.com>
2019-05-01 19:33:33 +0100

    Added auth0 and other pages

--- app.js
+++ app.js
@@ -27,6 +27,33 @@ app.use(session({
     saveUninitialized: true
 }));
 
+// Configure Passport to use Auth0
+var strategy = new Auth0Strategy(
+    {
+      domain: process.env.AUTH0_DOMAIN,
+      clientID: process.env.AUTH0_CLIENT_ID,
+      clientSecret: process.env.AUTH0_CLIENT_SECRET,
+      callbackURL:
+        process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback'
+    },
+    function (accessToken, refreshToken, extraParams, profile, done) {
+      // accessToken is the token to call Auth0 API (not needed in the most cases)
+      // extraParams.id_token has the JSON Web Token
+      // profile has all the information from the user
+      return done(null, profile);
+    }
+  );
+  
+passport.use(strategy);
+
+passport.serializeUser(function(user, done) {
+done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+done(null, user);
+});
+
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
@@ -34,7 +61,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 
 app.use((req, res, next) => {
-    const err = new Error('Not Found');
+    const err = new Error('Not Found');    
     err.status = 404;
     next(err);
 });
--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,61 @@
 
 const express = require('express');
+const passport = require('passport');
 const router = express.Router();
+const ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn();
+const request = require('request');
+
+const env = {
+    AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+    AUTH0_DOMAIN: process.env.AUTH0_DOMAIN
+};
 
 router.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', {env: env});
     // res.send('You are on the home page.');
 });
 
-router.get('/login', (req, res) => {
-    res.send('You are on the login page');
+router.get('/login', passport.authenticate('auth0', {
+    clientID: env.AUTH0_CLIENT_ID,
+    domain: env.AUTH0_DOMAIN,
+    redirectUri: "http://localhost:3000/callback",
+    responseType: 'code',
+    scope: 'openid profile email'
+}), (req, res) => {
+    // res.send('You are on the login page');
+    res.redirect('/');
 });
 
 router.get('/logout', (req, res) => {
-    res.send('You are on the logout page');
+    // res.send('You are on the logout page');
+    req.logout();
+    res.redirect('/');
 });
 
-router.get('/polls', (req, res) => {
-    res.send('You are on the polls page');
+router.get('/polls', ensureLoggedIn, (req, res) => {
+    let apiUrl = 'http://elections.huffingtonpost.com/pollster/api/charts.json?topic=2016-president'; 
+    request(apiUrl, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+            const polls = JSON.parse(body);
+            res.render('polls', {env: env, user: req.user, polls: polls});
+        } else {
+            res.render('error');
+        }
+    })
+    // res.render('user');
+    // res.send('You are on the user page');
 });
 
-router.get('/user', (req, res) => {
-    res.send('You are on the user page');
+router.get('/user', ensureLoggedIn, (req, res) => {
+    
+    res.render('user', {env: env, user: req.user});
+    // res.send('You are on the user page');
+});
+
+router.get('/callback', passport.authenticate('auth0', {failureRedirect: '/url-if-something-fails'}), (req, res) => {
+    
+    res.redirect(req.session.returnTo || '/polls');
+    // res.send('You are on the user page');
 });
 
 module.exports = router;
